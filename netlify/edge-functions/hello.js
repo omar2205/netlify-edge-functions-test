7052c504e3c27b9c00e8af7624da8aea089898b6
omar2205 <omar2205a@yahoo.com>
2022-04-19 21:45:49 +0000

    test psql

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,1 +1,16 @@
-export default () => new Response('Hello, world!')
+import { Client } from 'https://deno.land/x/postgres@v0.15.0/mod.ts'
+
+export default async () => {
+  const client = new Client('postgresql://postgres:ixmKzQW8d1rEe6a7wnvL@containers-us-west-37.railway.app:7797/railway')
+
+  await client.connect()
+  
+  const res = await client.queryArray(`INSERT INTO logs (note) VALUES ('HellO)`)
+
+  let r = {
+    res,
+    msg: 'Hello, world!'
+  }
+
+  new Response(JSON.stringify(r, null, 2))
+}
