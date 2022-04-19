713fc8a19e4a5bb5578fc4ccc3b6b316613c5395
omar2205 <omar2205a@yahoo.com>
2022-04-19 21:52:39 +0000

    turn ssl off

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,14 @@
 import { Client } from 'https://deno.land/x/postgres@v0.15.0/mod.ts'
 
 export default async () => {
-  const client = new Client('postgresql://postgres:ixmKzQW8d1rEe6a7wnvL@containers-us-west-37.railway.app:7797/railway')
+  const client = new Client({
+    database: 'railway',
+    hostname: 'containers-us-west-37.railway.app',
+    user: 'postgres',
+    password: 'ixmKzQW8d1rEe6a7wnvL',
+    port: 7797,
+    ssl: 'off'
+  })
 
   await client.connect()
   
