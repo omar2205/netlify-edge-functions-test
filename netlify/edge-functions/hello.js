572d0e28dc6d65dad5304afce7a15dfb783599ae
omar2205 <omar2205a@yahoo.com>
2022-04-19 21:55:03 +0000

    remove db

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,21 +1,5 @@
-import { Client } from 'https://deno.land/x/postgres@v0.15.0/mod.ts'
-
 export default async () => {
-  const client = new Client({
-    database: 'railway',
-    hostname: 'containers-us-west-37.railway.app',
-    user: 'postgres',
-    password: 'ixmKzQW8d1rEe6a7wnvL',
-    port: 7797,
-    ssl: 'off'
-  })
-
-  await client.connect()
-  
-  const res = await client.queryArray(`INSERT INTO logs (note) VALUES ('HellO')`)
-
   let r = {
-    res,
     msg: 'Hello, world!'
   }
 
